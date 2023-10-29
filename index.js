13831437761db20e74dc823f43ebc5586c8b6cf7
Ivelina Plocheva <ivelina.plocheva@newredo.com>
2023-10-29 16:35:23 +0000

    Move word to api folder.

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const word = require("./api/word");
 
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 })
 
-
-// Creating a new endpoint to fetch the word from the external API
-app.get('/word', async (req, res) => {
-    console.log("In get word");
-    console.log(process.env.API_KEY);
-  try {
-    const response = await axios.get('https://api.api-ninjas.com/v1/randomword?type=noun', {
-      headers: {
-        'X-Api-Key': process.env.API_KEY,
-      },
-    });
-
-    res.json(response.data); // Sending the data as a JSON response
-  } catch (error) {
-    res.status(500).send(error.message || error);
-  }
-});
-
+app.use("/api/word", word);
 
 app.listen(process.env.PORT || 3000);
 
